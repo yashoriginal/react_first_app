9e15a2d7647ecaa29cc9a946a1edf0a7785de1ae
yashoriginal <yaswanth.suriya@outlook.com>
2024-10-20 19:03:41 +0530

    Props passing funtions, strucuring components

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  const description = reactDescriptions[genRandomInt(2)];
-  return(
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-function CoreConcept({title, image , description}){
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect(){
+    console.log("Handle selected");
+  }
+
   return (
     <div> 
       <Header/>     
@@ -44,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id = "examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect = {handleSelect}>Components</TabButton>
+            <TabButton onSelect = {handleSelect}>JSX</TabButton>
+            <TabButton onSelect = {handleSelect}>Props</TabButton>
+            <TabButton onSelect = {handleSelect}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+export default function TabButton({children, onSelect}){
+
+    return(
+        <li><button onClick={onSelect}>{children}</button></li>
+    );
+}
