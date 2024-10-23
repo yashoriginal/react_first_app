34ade6b9855b6830e2c8969ee22b40b70507a7b9
yashoriginal <yaswanth.suriya@outlook.com>
2024-10-23 19:51:12 +0530

    Pattern and forwarding props

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,6 @@
-export default function TabButton({children, onSelect, isSelected}){
+export default function TabButton({children, isSelected, ...props}){
 
     return(
-        <li><button className = {isSelected ? "active" : undefined} onClick={onSelect}>{children}</button></li>
+        <li><button className = {isSelected ? "active" : undefined} {...props}>{children}</button></li>
     );
 }
