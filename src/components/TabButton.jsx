d32b695d430566f097bff128fea1e83aa6026988
yashoriginal <yaswanth.suriya@outlook.com>
2024-10-23 10:46:22 +0530

    Dynamic styles and key

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,6 @@
-export default function TabButton({children, onSelect}){
+export default function TabButton({children, onSelect, isSelected}){
 
     return(
-        <li><button onClick={onSelect}>{children}</button></li>
+        <li><button className = {isSelected ? "active" : undefined} onClick={onSelect}>{children}</button></li>
     );
 }
